6ac08ed6d6d31fda30024e211785bf8a14827f5b
Daniel Glover <iamglover02@gmail.com>
2023-11-14 17:20:31 +0000

    Scaled Hero section for larger screens

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -16,8 +16,10 @@ const divStyle: React.CSSProperties = {
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     width: '100%',
-    backgroundColor: 'linear-gradient(0deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0.25) 100%)',
+  };
 
+  const innerBG: React.CSSProperties = {
+    backgroundColor: 'linear-gradient(0deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0.25) 100%)',
   };
 
 const Hero: React.FC = () => {
@@ -28,13 +30,17 @@ const Hero: React.FC = () => {
             <span className="SFRegular text-[#DCDCDC]">Introducing Raft Cards</span>
             <Image src="/svgs/chevron-right.svg" alt="" width={20} height={20}/>
         </div>
-        <span className="SFRegular text-4xl text-center">Building the future of banking.</span>
-        <span className="SFRegular text-[#BDBDBD] text-center">Experience the future of banking with RAFT. We&apos;re here to empower your financial journey.</span>
+        <span className="SFRegular text-4xl lg:text-7xl lg:w-[40rem] lg:leading-[5rem] text-center">Building the future of banking.</span>
+        <span className="SFRegular text-[#BDBDBD] lg:w-[35rem] lg:text-2xl lg:leading-9 text-center">Experience the future of banking with RAFT. We&apos;re here to empower your financial journey.</span>
 
         <button className="bg-[#2B892E] SFMedium rounded-[6rem] px-8 py-4">Get Started</button>
      </div>
 
-     <div style={heroImage} className="h-[24rem] w-full rounded-lg"></div>
+     <div style={heroImage} className="h-[24rem] lg:h-[35rem] w-full rounded-lg">
+        <div style={innerBG} className="inner w-full h-full">
+
+        </div>
+     </div>
       
     </div>
   );
